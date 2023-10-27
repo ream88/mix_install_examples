386f8c072a529ae41f21394b88ace45a4dcd7a1f
Mario Uher <uher.mario@gmail.com>
2023-10-27 19:23:00 +0200

    Fix a small typo

--- bandit.exs
+++ bandit.exs
@@ -20,7 +20,7 @@ end
 bandit = {Bandit, plug: Router, scheme: :http, options: [port: 4000]}
 {:ok, _} = Supervisor.start_link([bandit], strategy: :one_for_one)
 
-# unless running from IEx, sleep idenfinitely so we can serve requests
+# unless running from IEx, sleep indefinitely so we can serve requests
 unless IEx.started?() do
   Process.sleep(:infinity)
 end
